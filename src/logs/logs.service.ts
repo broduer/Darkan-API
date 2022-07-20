ae5505ea61525ccf81e197c36daf4ff7280a914c
Trenton Kress <trenton.kress@gmail.com>
2022-07-20 15:54:28 -0700

    Cannot find module??

--- src/logs/logs.service.ts
+++ src/logs/logs.service.ts
@@ -1,5 +1,5 @@
 import { Inject, Injectable } from '@nestjs/common';
-import { MONGODB_PROVIDER } from '../constants';
+import { MONGODB_PROVIDER } from 'src/constants';
 
 @Injectable()
 export class LogsService {
